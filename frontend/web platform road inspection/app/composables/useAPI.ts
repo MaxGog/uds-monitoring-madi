05ccb469a2bffc289838fa4ba106f0d46e91df73
Atomeisbrecher <stratii05@mail.ru>
2026-07-05 21:32:57 +0300

    Забыл убрать лишний навигатор

--- frontend/web platform road inspection/app/composables/useAPI.ts
+++ frontend/web platform road inspection/app/composables/useAPI.ts
@@ -25,37 +25,48 @@ export const apiFetch = $fetch.create({
     console.log("Поймали ошибку:", response.status);
     const accessToken = useState("access_token");
 
-    if (response.status === 401 && !request.toString().includes("/auth/refresh")) {
+    if (
+      response.status === 401 &&
+      !request.toString().includes("/auth/refresh")
+    ) {
       console.log("Это 401, пробуем обновить...");
       if (!refreshPromise) {
         refreshPromise = (async () => {
-      try {
-        const newTokens = await $fetch<{ access_token: string }>(`${BACKEND_URL}/auth/refresh`, {
-          method: "POST",
-          credentials: 'include',
-          headers: {
-            "Authorization": `Bearer ${accessToken.value}`
-          },
-        });
+          try {
+            const newTokens = await $fetch<{ access_token: string }>(
+              `${BACKEND_URL}/auth/refresh`,
+              {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                  Authorization: `Bearer ${accessToken.value}`,
+                },
+              },
+            );
 
-        accessToken.value = newTokens.access_token;
-        // Повторяем исходный запрос
-        options.headers = new Headers(options.headers);
-        options.headers.set("Authorization", `Bearer ${newTokens.access_token}`);
-        return $fetch(request, options);
-      } catch (refreshError) {
-        console.log('Refresh токен протух или отсутствует, выкидываем на логин');
-        const accessToken = useState("access_token");
-        accessToken.value = null;
-        return navigateTo("/login");
-      } finally {
-        refreshPromise = null;
+            accessToken.value = newTokens.access_token;
+            // Повторяем исходный запрос
+            options.headers = new Headers(options.headers);
+            options.headers.set(
+              "Authorization",
+              `Bearer ${newTokens.access_token}`,
+            );
+            return $fetch(request, options);
+          } catch (refreshError) {
+            console.log(
+              "Refresh токен протух или отсутствует, выкидываем на логин",
+            );
+            const accessToken = useState("access_token");
+            accessToken.value = null;
+            return navigateTo("/login");
+          } finally {
+            refreshPromise = null;
+          }
+        })();
       }
-    })()
-  }
     } else {
       // Нет токена — редирект на логин
-      navigateTo("/login");
+      // navigateTo("/login");
     }
   },
 });
